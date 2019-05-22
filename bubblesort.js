668574b67d372bfb80eec8dcea86c1c1ce1c4342
dwyfrequency <jdwy215@gmail.com>
2019-05-22 13:07:03 -0400

    added completed solution except for that rascal jasmine

--- bubblesort.js
+++ bubblesort.js
@@ -21,10 +21,43 @@ const bubbleSort = arr => {
   return arr;
 };
 
-console.log(bubbleSort([1]));
+const bubbleSortWithCallback = (arr, comparatorFunc) => {
+  if (comparatorFunc === undefined) {
+    let sorted = false;
+    let counter = 1;
+    while (!sorted) {
+      sorted = true;
+      for (let i = 0; i < arr.length - counter; i++) {
+        if (arr[i] > arr[i + 1]) {
+          swap(i, i + 1, arr);
+          sorted = false;
+        }
+      }
+      counter++;
+    }
+    return arr;
+  } else {
+    let sorted = false;
+    let counter = 1;
+    while (!sorted) {
+      sorted = true;
+      for (let i = 0; i < arr.length - counter; i++) {
+        if (comparatorFunc(arr[i], arr[i + 1]) > 0) {
+          swap(i, i + 1, arr);
+          sorted = false;
+        }
+      }
+      counter++;
+    }
+    return arr;
+  }
+};
+// console.log(bubbleSort([1]));
 
-console.log(bubbleSort([2, 1]));
+// console.log(bubbleSort([2, 1]));
 
-console.log(bubbleSort([5, 1, -1, 10, 9]));
+console.log(bubbleSortWithCallback([5, 1, -1, 10, 9], (a, b) => a - b));
 
 // module.exports = bubbleSort;
+
+// module.exports = { bubbleSort, swap };
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,30 +1,36 @@
-const { bubbleSort, swap } = require('../bubblesort');
+// const { bubbleSort, swap } = require('../bubblesort')
 
+// const bubbleSort = require('../bubblesort');
 describe('Bubble Sort', function() {
   const bubbleSpy = {};
-  bubbleSpy.swap = swap;
   beforeAll(function() {
+    bubbleSpy.swap = swap;
     spyOn(bubbleSpy, 'swap').and.callThrough();
   });
 
   it('handles an empty array', function() {
+    // console.log(bubbleSpy.swap(0, 1, [2, 1]));
     expect(bubbleSort([])).toEqual([]);
     expect(bubbleSpy.swap.calls.count()).toEqual(0);
+    // expect(bubbleSpy.swap.callCount).toEqual(0);
   });
 
   it('handles an array of one', function() {
     expect(bubbleSort([1])).toEqual([1]);
     expect(bubbleSpy.swap.calls.count()).toEqual(0);
+    // expect(bubbleSpy.swap.callCount).toEqual(0);
   });
 
   it('properly sorts the array of two', function() {
     expect(bubbleSort([5, 1])).toEqual([1, 5]);
     expect(bubbleSpy.swap.calls.count()).toEqual(1);
+    // expect(bubbleSpy.swap.callCount).toEqual(1);
   });
 
   it('properly sorts an array of multiple', function() {
     expect(bubbleSort([5, 1, -1])).toEqual([-1, 1, 5]);
     expect(bubbleSpy.swap.calls.count()).toEqual(3);
+    // expect(bubbleSpy.swap.callCount).toEqual(3);
   });
 
   it('properly sorts an array of multiple', function() {
